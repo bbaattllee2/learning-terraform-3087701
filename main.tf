8036d94f71a0805eb33a4ec92b7c0d29d3885069
James Slocum <102326459+bbaattllee2@users.noreply.github.com>
2023-03-31 16:44:19 -0500

    Uses module for security group

--- main.tf
+++ main.tf
@@ -22,11 +22,25 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [module.web_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "web_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "http-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "web" {
@@ -34,35 +48,4 @@ resource "aws_security_group" "web" {
   description = "Allow http and https in. Allow everything out"
 
   vpc_id = data.aws_vpc.default.id
-}
-
-
-resource "aws_security_group_rule" "web_http_in" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_https_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_everything_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
 }
