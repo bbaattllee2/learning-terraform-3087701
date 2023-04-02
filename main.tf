96239da3a464ad6594214f0a5b043fabde09f372
James Slocum <102326459+bbaattllee2@users.noreply.github.com>
2023-04-02 13:05:58 -0500

    Replaces EC2 with autoscaling

--- main.tf
+++ main.tf
@@ -18,16 +18,20 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  # insert the 1 required variable here
+
+  name     = "web"
+  min_size = "1"
+  max_size = "2"
+  vpc_zone_identifier = module.web_vpc.public_subnets
+  target_groups_arns  = module.web_alb.target_groups_arns
+  security_groups     = [module.web_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  subnet_id = module.web_vpc.public_subnets[0]
-
-  vpc_security_group_ids = [module.web_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
 module "web_vpc" {
@@ -62,7 +66,7 @@ module "web_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-module "alb" {
+module "web_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
