4bc56c1afef2b3478481d88c10d7ab98a1d379f6
James Slocum <102326459+bbaattllee2@users.noreply.github.com>
2023-04-02 13:38:34 -0500

    Fix typos

--- main.tf
+++ main.tf
@@ -37,11 +37,11 @@ module "autoscaling" {
 module "web_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "${var.environment.name_prefix}.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["${var.environment.name_prefix}.101.0/24", "${var.environment.name_prefix}.102.0/24", "${var.environment.name_prefix}.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
